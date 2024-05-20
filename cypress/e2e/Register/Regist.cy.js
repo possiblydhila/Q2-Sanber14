875d45ea7607121136f303c8cbf307d5d5790f34
AlokaAryayudha <Aaloka221@gmail.com>
2024-05-20 20:37:51 +0700

    Update Regist

--- cypress/e2e/Register/Regist.cy.js
+++ cypress/e2e/Register/Regist.cy.js
@@ -1,4 +1,6 @@
-import RegistPage from "../../support/pageObjectRegist/RegistPage";
+import NavigationPage from "../../support/Navigator/Navigation";
+import DataInvalid from "../../fixtures/DataInvalid.json";
+import UserData from "../../fixtures/UserData.json";
 
 describe('Regist', () => {
 
@@ -21,105 +23,81 @@ describe('Regist', () => {
   const LastName = RandomLast();
   const EmailDummy = RandomEmail();
 
-
-  it('Random name', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
-    cy.nama(FirstName,LastName )
+  // Positive Test
+  it('TC - 1 : Berhasil Registrasi', () => {
+    cy.homepage()
+    cy.nama(FirstName,LastName)
     cy.ketikemail(EmailDummy,'A@1234567')
     cy.url().should('include','customer')
   })
   
-  it('Berhasil Registrasi', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
-    cy.nama(FirstName,LastName )
-    cy.ketikemail(EmailDummy,'A@1234567')
-    cy.url().should('include','customer')
-  })
-
-  it('Email sudah digunakan', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
+  // Negative Test
+  it('TC - 2 : Email telah digunakan', () => {
+    cy.homepage()
     cy.nama(FirstName,LastName)
-    cy.ketikemail('dummy22@gmail.com','A@123456')
-   // cy.get('.message-error > div').should('contain.text','There is already an account with this email address. If you are sure that it is your email address, ')
+    cy.ketikemail('Dummy@gmail.com','A@123456')
     cy.kosong('.message-error > div','There is already an account with this email address. If you are sure that it is your email address,')
   })
 
-  it('Nama Akhir kosong', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
-    cy.get('#firstname').type(FirstName)
-    cy.ketikemail('asdasdd@gmail.com','A@123456')
+    // Negative Test
+  it('TC - 3 : Nama Akhir kosong', () => {
+    cy.homepage()
+    cy.NoLast(FirstName,EmailDummy,'A@123456')
     cy.kosong('#maincontent','This is a required field.')
   })
-
-  it('Nama Awal kosong', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
-    cy.get('#lastname').type(LastName)
-    cy.ketikemail('asdasdd@gmail.com','A@123456')
-    //cy.get('#maincontent').should('contain.text','This is a required field.')
+    // Negative Test
+  it('TC - 4 : Nama Awal Kosong', () => {
+    cy.homepage()
+    cy.NoFirst(LastName,EmailDummy,'A@123456')
     cy.kosong('#maincontent','This is a required field.')
-    
   })
- 
-  it('Email Salah - Command', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
+    // Negative Test
+  it(' TC - 5 : Email Salah - Command', () => {
+    cy.homepage()
     cy.nama(FirstName,LastName)
     cy.ketikemail('asdasd$gmail.com','A@123456')
     cy.kosong('#maincontent','Please enter a valid email address (Ex: johndoe@domain.com).')
   })
 
-
-  it('Password tanpa symbol', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
+    // Negative Test 
+  it('TC - 6 : Password tanpa symbol', () => {
+    cy.homepage()
     cy.nama(FirstName,LastName)
     cy.ketikemail(EmailDummy,'12313213')
     cy.kosong('#maincontent','Minimum of different classes of characters in password is 3. Classes of characters: Lower Case, Upper Case, Digits, Special Characters.')
   })
 
-  it('Password Berbeda', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
+    // Negative Test
+  it('TC - 7 : Password Berbeda', () => {
+    cy.homepage()
     cy.nama(FirstName,LastName)
-    cy.ketik1(EmailDummy,'A@123456','A@113456')
+    cy.Passwrd(EmailDummy,'A@123456','A@113456')
     cy.kosong('#maincontent','Please enter the same value again.')
   })
  
-  it('Password kurang dari 8 - Manual', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
-    cy.get('#firstname').type('Dummy')
-    cy.get('#lastname').type('dumdum')
-    cy.get('#email_address').type('dummy22@gmail.com')
-    cy.get('#password').type('A@1234')
-    cy.get('#password-confirmation').type('A@1234')
-    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+    // Negative Test
+  it('TC - 8 : Password kurang dari 8 ', () => {
+    cy.homepage()
+    cy.data(FirstName,LastName,EmailDummy,'A@1234','A@1234')
     cy.get('#maincontent')
     .should('contain.text','Minimum length of this field must be equal or greater than 8 symbols. Leading and trailing spaces will be ignored.')
   })
 
-  it('Testing Fixtures', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
-    // cy.fixture('userDataRegist.json').then((users) => {
-    //   cy.data(users.namaawal,users.namaakhir,users.email,users.pssw)
-    //   cy.url().should('include','customer') (Jika tidak menggunakan array)
-     cy.fixture('userDataRegist.json').then((users) => {
-      const userDataRegist = users[1]
-      cy.data(userDataRegist.namaawal, userDataRegist.namaakhir, userDataRegist.email, userDataRegist.pssw)
+  // Positive Test with Fixture
+  it('TC - 9 :Testing Valid', () => {
+    cy.homepage()
+     cy.fixture('UserData.json').then((users) => {
+      const UserData = users[1]
+      cy.data(UserData.namaawal, UserData.namaakhir, UserData.email, UserData.pssw)
       cy.url().should('include','customer') 
     })
   })
 
-  it('Testing Fixtures', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get('.panel > .header > :nth-child(3) > a').click()
-     cy.fixture('DataRegist.json').then((data) => {
+  
+  // Positive Test with Fixture
+  it('TC - 10 :Testing Invalid Data', () => {
+    cy.homepage()
+     cy.fixture('DataInvalid.json').then((data) => {
       data.invalid.forEach((userdata) => {
         console.log(userdata.invalid_awal, userdata.invalid_akhir, userdata.invalid_user, userdata.invalid_pass);
         cy.get('#maincontent').should('be.visible')
@@ -127,27 +105,45 @@ describe('Regist', () => {
     })
   })
 
-  it.only('Test POM 1', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    cy.get(RegistPage.nama_awl).type(FirstName)//cy.nama(FirstName,LastName)
-    cy.get(RegistPage.nama_akh).type(LastName)
-    cy.get(RegistPage.email_add).type(EmailDummy)
-    cy.get(RegistPage.passwd).type('sadasd@Asda123')
-    cy.get(RegistPage.repasswd).type('sadasd@Asda123')
-    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
-    cy.get(RegistPage.error_msg).should('be.visible') //Kalau salah menggunakan error_msg
-    //cy.url().should('include','customer') 
-  })
 
-  it('Test POM 2', () => {
+  // Positive Test Sukses mendaftar
+  it('TC - 11 : Berhasil Mendaftar', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
-    RegistPage.inputNama(FirstName)
-    RegistPage.inputNamaakhir(LastName)
-    RegistPage.inputEmail(EmailDummy)
-    RegistPage.inputPass('A@1234567')
-    RegistPage.inputRepass('A@1234567')
-    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
-    cy.url().should('include','customer') 
-  })
+    const navigationPage = new NavigationPage();
+    const userData = UserData.DataUser2;
+    navigationPage.inputNama(userData.firstnama);
+    navigationPage.inputNamaakhir(userData.last);
+    navigationPage.inputEmail(userData.email);
+    navigationPage.inputPass(userData.passwd);
+    navigationPage.inputRepass(userData.cpasswd);
+    navigationPage.inputButton(); 
+    navigationPage.verifError();
+  });
 
+  // Positive Test Sukses mendaftar
+  it.only('TC - 12 : First Name', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+    const navigationPage = new NavigationPage();
+    const userData = UserData.DataUser2;
+    navigationPage.inputNamaakhir(userData.last);
+    navigationPage.inputEmail(userData.email);
+    navigationPage.inputPass(userData.passwd);
+    navigationPage.inputRepass(userData.cpasswd);
+    navigationPage.inputButton(); 
+    navigationPage.verifError();
+  });
+
+  
+  // Negative Test Sukses mendaftar
+  it('TC - 13 : Lastname tidak ada', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+    const navigationPage = new NavigationPage();
+    const userData = UserData.DataUser2;
+    navigationPage.inputNama(userData.firstnama);
+    navigationPage.inputEmail(userData.email);
+    navigationPage.inputPass(userData.passwd);
+    navigationPage.inputRepass(userData.cpasswd);
+    navigationPage.inputButton(); 
+    navigationPage.verifError();
+  });
 })
