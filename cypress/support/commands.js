e0491a89afb41b02a1f9e3f5e8137a36d5553061
AlokaAryayudha <Aaloka221@gmail.com>
2024-05-20 20:39:41 +0700

    Update Command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,8 @@
 
+Cypress.Commands.add('homepage', () => {
+    cy.visit('https://magento.softwaretestingboard.com/');
+});
+
 Cypress.Commands.add('ketikemail', (useremail,userpassw) => {
     cy.get('#email_address').clear().type(useremail)
     cy.get('#password').clear().type(userpassw)
@@ -6,7 +10,26 @@ Cypress.Commands.add('ketikemail', (useremail,userpassw) => {
     cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
 })
 
+Cypress.Commands.add('NoLast', (awal,useremail,userpassw) => {
+    cy.get('.panel > .header > :nth-child(3) > a').click()
+    cy.get('#firstname').clear().type(awal)
+    cy.get('#email_address').clear().type(useremail)
+    cy.get('#password').clear().type(userpassw)
+    cy.get('#password-confirmation').clear().type(userpassw)
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+})
+
+Cypress.Commands.add('NoFirst', (akhir,useremail,userpassw) => {
+    cy.get('.panel > .header > :nth-child(3) > a').click()
+    cy.get('#lastname').clear().type(akhir)
+    cy.get('#email_address').clear().type(useremail)
+    cy.get('#password').clear().type(userpassw)
+    cy.get('#password-confirmation').clear().type(userpassw)
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+})
+
 Cypress.Commands.add('data', (awal,akhir,useremail,userpassw) => {
+    cy.get('.panel > .header > :nth-child(3) > a').click()
     cy.get('#firstname').clear().type(awal)
     cy.get('#lastname').clear().type(akhir)
     cy.get('#email_address').clear().type(useremail)
@@ -14,17 +37,19 @@ Cypress.Commands.add('data', (awal,akhir,useremail,userpassw) => {
     cy.get('#password-confirmation').clear().type(userpassw)
     cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
 })
-Cypress.Commands.add('ketik1', (useremail,userpassw,salapass) => {
+Cypress.Commands.add('Passwrd', (useremail,userpassw,salapass) => {
+    cy.get('.panel > .header > :nth-child(3) > a').click()
     cy.get('#email_address').clear().type(useremail)
     cy.get('#password').clear().type(userpassw)
     cy.get('#password-confirmation').clear().type(salapass)
     cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
 })
 Cypress.Commands.add('nama', (awal,akhir) => {
+    cy.get('.panel > .header > :nth-child(3) > a').click()
     cy.get('#firstname').clear().type(awal)
     cy.get('#lastname').clear().type(akhir)
 })
 
-Cypress.Commands.add('kosong', (elemen,notif1) => {
-    cy.get(elemen).should('contain.text',notif1)
+Cypress.Commands.add('kosong', (elemen,notif) => {
+    cy.get(elemen).should('contain.text',notif)
 })
