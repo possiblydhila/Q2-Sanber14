bfcc009adabed0d2530d76af46ec1a3a42280a1e
AlokaAryayudha <Aaloka221@gmail.com>
2024-05-20 15:00:59 +0700

    Menambahkan PageObject dan Commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('ketikemail', (useremail,userpassw) => {
+    cy.get('#email_address').clear().type(useremail)
+    cy.get('#password').clear().type(userpassw)
+    cy.get('#password-confirmation').clear().type(userpassw)
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+})
+
+Cypress.Commands.add('data', (namaawal,akhir,useremail,userpassw) => {
+    cy.get('#firstname').clear().type(namaawal)
+    cy.get('#lastname').clear().type(akhir)
+    cy.get('#email_address').clear().type(useremail)
+    cy.get('#password').clear().type(userpassw)
+    cy.get('#password-confirmation').clear().type(userpassw)
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+})
+Cypress.Commands.add('ketik1', (useremail,userpassw,salapass) => {
+    cy.get('#email_address').clear().type(useremail)
+    cy.get('#password').clear().type(userpassw)
+    cy.get('#password-confirmation').clear().type(salapass)
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+})
+Cypress.Commands.add('nama', (awal,akhir) => {
+    cy.get('#firstname').clear().type(awal)
+    cy.get('#lastname').clear().type(akhir)
+})
+
+Cypress.Commands.add('kosong', (elemen,notif1) => {
+    cy.get(elemen).should('contain.text',notif1)
+})
