ee37e0f2b748983e67e5046ddf653454969baa3c
AlokaAryayudha <Aaloka221@gmail.com>
2024-05-20 10:32:49 +0700

    Update Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl : 'https://magento.softwaretestingboard.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 4500,
+    screenshotOnRunFailure: false,
+    pageLoadTimeout: 80000
   },
 });
