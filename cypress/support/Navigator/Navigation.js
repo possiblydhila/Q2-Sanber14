d1554f1c8d3de4346cfd7a13ddb61bf9817d00f9
AlokaAryayudha <Aaloka221@gmail.com>
2024-05-20 20:38:59 +0700

    Update Navigation Page

--- cypress/support/Navigator/Navigation.js
+++ cypress/support/Navigator/Navigation.js
@@ -1,35 +1,39 @@
-class NavigatorPage{
-    
+class NavigationPage {
     first = '#firstname'
     last = '#lastname'
     email = '#email_address'
     passwd = '#password'
     cpasswd = '#password-confirmation'
-    submit = '#submit'
+    submit = '#form-validate > .actions-toolbar > div.primary > .action'
     error_msg = '#maincontent'
 
-    inputNama(first){
+    inputNama(first) {
         cy.get(this.first).clear().type(first)
     }
-    inputNamaakhir(last){
+
+    inputNamaakhir(last) {
         cy.get(this.last).clear().type(last)
     }
-    inputEmail(email){
+
+    inputEmail(email) {
         cy.get(this.email).clear().type(email)
     }
-    inputPass(passwd){
+
+    inputPass(passwd) {
         cy.get(this.passwd).clear().type(passwd)
     }
-    inputRepass(cpasswd){
+
+    inputRepass(cpasswd) {
         cy.get(this.cpasswd).clear().type(cpasswd)
     }
-    InputButton(submit){
-        cy.cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
-    }
-    verifError(error_msg){
-        cy.get(this.error_msg).should('be.visible');
+
+    inputButton() {
+        cy.get(this.submit).click()
     }
 
+    verifError() {
+        cy.get(this.error_msg).should('be.visible')
+    }
 }
 
-export default new NavigatorPage()
+export default NavigationPage;
